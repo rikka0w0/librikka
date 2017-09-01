808a92190ec2896a8894f5059bbb566f3c3fa936
rikka0w0 <929513338@qq.com>
2017-09-01 18:22:07 +1000

    First Upload

---
+++ src/rikka/librikka/block/ISubBlock.java
@@ -0,0 +1,10 @@
+package rikka.librikka.block;
+
+public interface ISubBlock {
+    /**
+     * Only use for subBlocks
+     *
+     * @return an array of unlocalized names
+     */
+    String[] getSubBlockUnlocalizedNames();
+}
