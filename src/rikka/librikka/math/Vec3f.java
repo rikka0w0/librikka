c77bb12caf7e73411bd6997e20d5283530c7d57a
rikka0w0 <929513338@qq.com>
2017-09-06 19:15:23 +1000

    Vec3f

--- src/rikka/librikka/math/Vec3f.java
+++ src/rikka/librikka/math/Vec3f.java
@@ -1,38 +1,58 @@
 package rikka.librikka.math;
 
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3i;
 
 public class Vec3f {
     /**
      * X coordinate of Vec3D
      */
-    public final float xCoord;
+    public final float x;
     /**
      * Y coordinate of Vec3D
      */
-    public final float yCoord;
+    public final float y;
     /**
      * Z coordinate of Vec3D
      */
-    public final float zCoord;
+    public final float z;
 
     public Vec3f(float x, float y, float z) {
-        xCoord = x;
-        yCoord = y;
-        zCoord = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
-    public float distanceTo(Vec3f vec) {
-        float x = this.xCoord - vec.xCoord;
-        float y = this.yCoord - vec.yCoord;
-        float z = this.zCoord - vec.zCoord;
-        return MathHelper.sqrt(x * x + y * y + z * z);
+    public Vec3f add(int x, int y, int z) {
+        return new Vec3f(this.x + x, this.y + y, this.z + z);
     }
-
+    
     public Vec3f add(float x, float y, float z) {
-        return new Vec3f(this.xCoord + x, this.yCoord + y, this.zCoord + z);
+        return new Vec3f(this.x + x, this.y + y, this.z + z);
     }
 
+    public Vec3f add(Vec3i in) {
+    	return new Vec3f(this.x + in.getX(), this.y + in.getY(), this.z + in.getZ());
+    }
+    
+    public Vec3f add(Vec3f in) {
+    	return new Vec3f(this.x + in.x, this.y + in.y, this.z + in.z);
+    }
+    
+    public float distanceTo(Vec3f vec) {
+        float dx = this.x - vec.x;
+        float dy = this.y - vec.y;
+        float dz = this.z - vec.z;
+        return MathHelper.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+    
+    public float distanceTo(float x, float y, float z) {
+        float dx = this.x - x;
+        float dy = this.y - y;
+        float dz = this.z - z;
+        return MathHelper.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+    
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
@@ -40,21 +60,21 @@ public class Vec3f {
             return false;
         } else {
             Vec3f vec3f = (Vec3f) obj;
-            return Float.compare(vec3f.xCoord, xCoord) == 0 && Float.compare(vec3f.yCoord, yCoord) == 0 && Float.compare(vec3f.zCoord, zCoord) == 0;
+            return Float.compare(vec3f.x, x) == 0 && Float.compare(vec3f.y, y) == 0 && Float.compare(vec3f.z, z) == 0;
         }
     }
 
     public int hashCode() {
-        long j = Float.floatToIntBits(xCoord);
+        long j = Float.floatToIntBits(x);
         int i = (int) (j ^ j >>> 16);
-        j = Float.floatToIntBits(yCoord);
+        j = Float.floatToIntBits(y);
         i = 15 * i + (int) (j ^ j >>> 16);
-        j = Float.floatToIntBits(zCoord);
+        j = Float.floatToIntBits(z);
         i = 15 * i + (int) (j ^ j >>> 16);
         return i;
     }
 
     public String toString() {
-        return "(" + xCoord + ", " + yCoord + ", " + zCoord + ")";
+        return "(" + x + ", " + y + ", " + z + ")";
     }
 }
