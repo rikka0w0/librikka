95854b22059a9b18b1463c39e2634102ab820a57
rikka0w0 <929513338@qq.com>
2017-09-12 22:14:53 +1000

    Update LibRikka(AutoGuiHandler)

--- src/rikka/librikka/AutoGuiHandler.java
+++ src/rikka/librikka/AutoGuiHandler.java
@@ -13,25 +13,36 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import rikka.librikka.container.IContainerWithGui;
 import rikka.librikka.tileentity.IGuiProviderTile;
 
+/**
+ * A more automatic and object-oriented GuiHandler, ID 0 to 5 is reserved for EnumFacing, and ID>5 will be considered as custom Gui/Container
+ *
+ */
 public class AutoGuiHandler implements IGuiHandler{
 	@Override
 	public final Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
 		BlockPos pos = new BlockPos(x, y, z);
+		if (ID > 5)
+			return getContainer(ID, player, world, pos);
+		
         TileEntity te = world.getTileEntity(pos);
         
         if (te instanceof IGuiProviderTile)
             return ((IGuiProviderTile) te).getContainer(player, EnumFacing.getFront(ID));
         
-		return getContainer(ID, player, world, pos);
+		return null;
 	}
 
 	@Override
 	public final Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z) {
+		BlockPos pos = new BlockPos(x, y, z);
+		if (ID > 5)
+			return getGui(ID, player, world, new BlockPos(x, y, z));
+		
     	Object guiContainer = getServerGuiElement(ID, player, world, x, y, z);
     	if (guiContainer instanceof IContainerWithGui)
     		return ((IContainerWithGui) guiContainer).createGui();
     	
-		return getGui(ID, player, world, new BlockPos(x, y, z));
+		return null;
 	}
 	
 	/////////////////////////////////////////////////
